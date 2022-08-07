a114e944e7880db3ae6e834aa131f85754ae88eb
Beck <tmxhs285@gmail.com>
2022-08-07 18:04:42 +0900

    Initial Commit

---
+++ MemberList Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemberList Practice
+//
+//  Created by 백경호 on 2022/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
